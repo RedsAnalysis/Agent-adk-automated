c351d935d6ec3acc08c46fce803c1010d389392e
Red <redatfsu@gmail.com>
2025-05-26 05:40:49 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 import os
 from google.adk.models.lite_llm import LiteLlm
 
-os.environ["GOOGLE_API_KEY"] = GEMINI_API_KEY # <--- REPLACE
+os.environ["GOOGLE_API_KEY"] = GEMINI_API_KEY
 os.environ["GOOGLE_GENAI_USE_VERTEXAI"] = "False"
 MODEL_GEMINI_2_5_FLASH = "gemini/gemini-2.5-flash-preview-04-17"
 
